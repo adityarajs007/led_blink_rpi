e74098a3eef1a5593b1162eb571e00e6f1c98b98
Adityaraj Suresh <adityarajsuresh@gmail.com>
2024-11-23 23:34:42 -0700

    Testing updated SSH key handling

--- counter.cpp
+++ counter.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	for (int i = 0; i < 1000000; i++)
 	{
-		std::cout << "Count is (v1.2): " << i << std::endl;
+		std::cout << "Count is (v1.3): " << i << std::endl;
 		std::this_thread::sleep_for(std::chrono::milliseconds(200));
 	}
 	return 0;
