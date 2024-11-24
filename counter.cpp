b6f958da68e05988e8d9191aca07932d4d0b3549
Adityaraj Suresh <adityarajsuresh@gmail.com>
2024-11-23 22:08:02 -0700

    file name changed. New file uploaded

--- counter.cpp
+++ counter.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
-
+#include <thread>
+#include <chrono>
 int main()
 {
 	for (int i = 0; i < 1000000; i++)
 	{
 		std::cout << "Count is : " << i << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(200));
 	}
 	return 0;
 }
