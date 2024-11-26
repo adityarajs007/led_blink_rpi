8be67a7862b38062e963116259be59e24db571d0
Adityaraj Suresh <adityarajsuresh@gmail.com>
2024-11-25 21:42:02 -0700

    cpp updated

--- counter.cpp
+++ counter.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	for (int i = 0; i < 40; i++)
 	{
-		std::cout << "Count is (v1.5): " << i << std::endl;
+		std::cout << "Count is (v1.690075557): " << i << std::endl;
 		std::this_thread::sleep_for(std::chrono::milliseconds(200));
 	}
 	return 0;
