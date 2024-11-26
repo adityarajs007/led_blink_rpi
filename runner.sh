c438e93887c6c3df2d3133386e7ff7f26dad26eb
Adityaraj Suresh <adityarajsuresh@gmail.com>
2024-11-26 13:30:45 -0700

    added runner.sh to the code folders

---
+++ runner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd ~/projects/counter_project
+
+# Pull the latest changes from the repository
+git pull origin main
+
+# Compile the code with updated structure
+g++ -I./include src/main.cpp src/counter.cpp -o counter_app
+
+# Run the compiled application
+./counter_app
